4e002173244acd757f81b1c7d852d5d3a5f862c7
Valentina <west.77@mail.ru>
2023-02-12 01:25:01 +0300

    Task 1 and 2 changed

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+//ЭТО решение ЗАДАЧИ 34 из Task 1 Задайте массив заполненный случайными положительными трёхзначными числами.
+
+Console.WriteLine("Введите длину массива:  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+RandonNumbers(numbers);
+Console.WriteLine("В этом массиве: ");
+PrintArray(numbers);
+
+void RandonNumbers(int[] numbers)
+{
+    for(int i = 0; i < size; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+
+
+int count = 0;
+
+for (int x = 0; x < numbers.Length; x++)
+{
+if (numbers[x] % 2 == 0)
+count++;
+}
+Console.WriteLine($"из {numbers.Length} чисел, {count} четных");
+
+
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
